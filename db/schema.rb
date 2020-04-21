e122e08fe8ec476aa05d5469e246667b5b4827af
JKR007 <abduqahhor.muhamadjonov@gmail.com>
2020-04-21 13:15:18 +0500

    create card_types

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_075036) do
+ActiveRecord::Schema.define(version: 2020_04_21_081238) do
+
+  create_table "card_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "display_name", null: false
+    t.index ["name"], name: "index_card_types_on_name", unique: true
+  end
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
