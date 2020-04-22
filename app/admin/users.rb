4ab717251109b647c65dcaca88f6d6717aa75ec6
JKR007 <abduqahhor.muhamadjonov@gmail.com>
2020-04-22 11:32:59 +0500

    integrate flattened_active_admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.register User do
 
+  index do
+    column :id
+    column :first_name
+    column :last_name
+    column :email
+    column :status
+    column :company
+    actions
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
