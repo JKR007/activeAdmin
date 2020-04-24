c1e05f62b450e90035dfaad040eadcb38725972f
JKR007 <abduqahhor.muhamadjonov@gmail.com>
2020-04-24 15:18:37 +0500

    generate company at admin

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ApplicationRecord
+  scope :active_companies, -> {where(status: :active)}
+  scope :inactive_companies, -> {where(status: :active)}
   has_many :users
   has_many :card_types, class_name: 'CompanyCardType', dependent: :destroy
   has_many :category_versions, class_name: 'CompanyCategoryVersion', dependent: :destroy
